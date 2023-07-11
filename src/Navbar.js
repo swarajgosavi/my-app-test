b9f507385b18a932887ab93efcd0882defe75467
Swaraj Gosavi <106607805+swarajgosavi@users.noreply.github.com>
2023-07-11 16:47:54 +0530

    setup gh-pages modified

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+import './App.css';
 
 export default function Navbar() {
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
+        <div className="App">
+            <header className="App-header">
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </header>
         </div>
     )
 }
